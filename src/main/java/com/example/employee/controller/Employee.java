7c7c8079d8cbf3da2a23f6ab4554b46de40ef265
KimkuyNgo <kngo2@paragoniu.edu.kh>
2026-03-04 01:12:48 +0700

    Added employee

---
+++ src/main/java/com/example/employee/controller/Employee.java
@@ -0,0 +1,20 @@
+package com.example.employee.controller;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+    private Double salary;
+
+}
