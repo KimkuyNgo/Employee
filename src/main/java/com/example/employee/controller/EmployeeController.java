f753e6c54b5fc1168a34823c3c2dfb17150365b8
KimkuyNgo <kngo2@paragoniu.edu.kh>
2026-03-04 01:35:49 +0700

    changed controller

--- src/main/java/com/example/employee/controller/EmployeeController.java
+++ src/main/java/com/example/employee/controller/EmployeeController.java
@@ -1,16 +1,39 @@
 package com.example.employee.controller;
 
+import ch.qos.logback.core.model.Model;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-@org.springframework.stereotype.Controller
-@Slf4j
+import java.security.Principal;
+import java.util.Optional;
+
+@Controller
 public class EmployeeController {
 
-    @GetMapping({"register.html"})
-    public String showregister() {
-        return "register";
+    @Autowired
+    private repoInterface.EmployeeRepository repo;
+
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
+    @GetMapping("/register")
+    public String showRegister() { return "register"; }
+
+    @PostMapping("/register")
+    public String register(Employee emp) {
+        emp.setPassword(encoder.encode(emp.getPassword()));
+        repo.save(emp);
+        return "redirect:/login";
     }
 
-
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, Principal principal) {
+        Optional<Employee> emp = repo.findByEmail(principal.getName());
+        model.addAttribute("employee", emp);
+        return "dashboard";
+    }
 }
